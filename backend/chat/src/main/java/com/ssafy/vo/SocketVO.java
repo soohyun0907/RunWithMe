7e5bb322aa7a627a0eefe39fe07eeb1e9554246b
이선수 <tjstndi77@naver.com>
2020-10-13 21:52:39 +0900

    Feat group chatting system demo ver
    Former-commit-id: 499f5de594eb032fb0d094e9b4084b4aa0175d2b

---
+++ backend/chat/src/main/java/com/ssafy/vo/SocketVO.java
@@ -0,0 +1,13 @@
+package com.ssafy.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SocketVO {
+
+	private String userName;
+	private String content;
+	
+}
