ffac3492995190e24f1323a3840fd1a739dac9fc
TAEK <hyungtaik@naver.com>
2020-10-15 09:49:40 +0900

    Feat Group Chatting Test | S03P31A303-17
    Former-commit-id: 462a895c1ec1d39687dd8e88da0155957348069a

---
+++ backend/RunWithMe/src/main/java/kr/co/rvm/handler/WebSocketChatHandler.java
@@ -0,0 +1,30 @@
+package kr.co.rvm.handler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kr.co.rvm.model.ChatMessage;
+import kr.co.rvm.model.ChatRoom;
+import kr.co.rvm.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WebSocketChatHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
+    }
+}
---
+++ backend/RunWithMe/src/test/java/kr/co/rvm/RunWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.rvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
