d32e1b2320500ae528346d4bd72f9ab7f9f6ad0a
전수현 <soohyun0907@naver.com>
2020-10-19 20:06:43 +0900

    Feat mark current location | S03P31A303-66
    Former-commit-id: 037252405ffef0b853874ab098747f9e84f6966d

--- frontend/RunWithMe/src/views/app/runnings/kakaomap_sh.vue
+++ frontend/RunWithMe/src/views/app/runnings/kakaomap_sh.vue
@@ -2,10 +2,10 @@
   <div class="main-content">
     <breadcumb :page="'KakaoMap'" :folder="'Pages'" />
 
-    <div id="map"></div>
+    <div id="map" ref="map"></div>
 
-    <!-- <section class="bottom-bar">
-      <div class="latLngLabel">{{lat}}, {{lng}}</div>
+    <section class="bottom-bar">
+      <div class="latLngLabel">{{lat}}, {{lon}}</div>
       <button class="ui button green" @click="startLocationUpdates">
         <i class="circle dot outline icon large"></i>
           Start Location
@@ -14,108 +14,122 @@
         <i class="circle dot outline icon large"></i>
           Stop Location
       </button>
-    </section> -->
+    </section>
 
   </div>
 </template>
 <script>
 export default {
-  name: "kakaomap",
-  data() {
-    return {
-      map: ""
-    };
-  },
+  name: 'kakaomap',
   mounted() {
-    window.kakao && window.kakao.maps ? this.initMap() : this.addScript();
-  },
-  methods: {
-    addScript(){
-      // console.log(this.kakaomap);
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
       const script = document.createElement('script');
-      /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap);
       script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=f1c07d2de50ad211c7c1df94eb1eaf69';
-            document.head.appendChild(script);
-    },
+      document.head.appendChild(script);
+    }
+  },
+  data() {
+    return {
+      map: null,
+      lat: 0,
+      lon: 0,
+      watchPositionId: null,
+      polyline: {},
+    }
+  },
+  methods: {
     initMap() {
-      // console.log(this.kakaomap);
-      let container = document.getElementById('map');
-      let lat = 33.450701,
-            lon = 126.570667;
+      const container = this.$refs.map;
+      var lat = 33.450701,
+          lon = 126.570667;
+      var options = {
+        center: new kakao.maps.LatLng(lat, lon),
+      };
 
-      // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+      var map = new kakao.maps.Map(container, options);
+      
+      // // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
       if (navigator.geolocation) {
         // GeoLocation을 이용해서 접속 위치를 얻어옵니다
         navigator.geolocation.getCurrentPosition(function(position) {
           lat = position.coords.latitude; // 위도
           lon = position.coords.longitude; // 경도
 
-          let locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-              message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+          var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
         
-          console.log("geolocation " + lat, lon);
-          let options = {
+          options = {
             center: new kakao.maps.LatLng(lat, lon),
-            level: 3
           };
 
-          this.map = new kakao.maps.Map(container, options);
-          // console.log(map);
-          // this.map = map;
-          console.log(this.map);
-
-          // 마커와 인포윈도우를 표시합니다
-          let marker = new kakao.maps.Marker({  
-            map: map, 
-            position: locPosition
+          map = new kakao.maps.Map(container, options);
+          
+          var circle = new kakao.maps.Circle({
+            center : new kakao.maps.LatLng(lat, lon),  // 원의 중심좌표 입니다 
+            radius: 50, // 미터 단위의 원의 반지름입니다 
+            strokeWeight: 5, // 선의 두께입니다 
+            strokeColor: '#75B8FA', // 선의 색깔입니다
+            strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+            strokeStyle: 'dashed', // 선의 스타일 입니다
+            fillColor: '#CFE7FF', // 채우기 색깔입니다
+            fillOpacity: 0.7  // 채우기 불투명도 입니다   
           }); 
-          let iwContent = message, // 인포윈도우에 표시할 내용
-              iwRemoveable = true;
-
-          // 인포윈도우를 생성합니다
-          let infowindow = new kakao.maps.InfoWindow({
-              content : iwContent,
-              removable : iwRemoveable
-          });
-    
-          // 인포윈도우를 마커위에 표시합니다 
-          infowindow.open(map, marker);
-    
+          
+          circle.setMap(map);
           // 지도 중심좌표를 접속위치로 변경합니다
           map.setCenter(locPosition);
-          // this.displayMarker(locPosition, message, kakaomap);
+          // console.log(map);
         });
-      } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-        // var locPosition = new kakao.maps.LatLng(33.450701, 126.570667), message = 'geolocation을 사용할수 없어요..'
-        // this.displayMarker(locPosition, message);
-        // console.log("else2 " + lat, lon);
       }
-      
+      this.map = map;
+      this.lat = lat;
+      this.lon = lon;
     },
     startLocationUpdates() {
-      
-      var marker = new google.maps.Marker({
-        map: this.kakaomap
-      });
+      var linePath = [new kakao.maps.LatLng(this.lat, this.lon)];
 
-      this.watchPositionId = navigator.geolocation.watchPosition(
-        position => {
-          this.lat = position.coords.latitude;
-          this.lng = position.coords.longitude;
-          console.log(position);
-          map.setCenter(new google.maps.LatLng(this.lat, this.lng)); 
-          marker.setPosition(new google.maps.LatLng(this.lat, this.lng));
-        },
-        error => {
-          console.log(error.message);
-        },
-        {timeout: 5000,
-          maximumAge: 0}
-      );
-            
+      this.watchPositionId = navigator.geolocation.watchPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+        
+        
+        console.log(position);
+
+        linePath.push(new kakao.maps.LatLng(this.lat, this.lon));
+        const container = this.$refs.map;
+        var options = {
+          center: new kakao.maps.LatLng(this.lat, this.lon),
+        };
+
+        var map = new kakao.maps.Map(container, options);
+        map.setCenter(new kakao.maps.LatLng(this.lat, this.lon)); 
+
+        // 지도에 표시할 선을 생성합니다
+        var polyline = new kakao.maps.Polyline({
+          path: linePath, // 선을 구성하는 좌표배열 입니다
+          strokeWeight: 5, // 선의 두께 입니다
+          strokeColor: '#FFAE00', // 선의 색깔입니다
+          strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+          strokeStyle: 'solid' // 선의 스타일입니다
+        });
+
+        console.log(linePath);
+        // 지도에 선을 표시합니다 
+        polyline.setMap(map);  
+        this.polyline = polyline;
+      },
+      error => {
+        console.log(error.message);
+      },
+      {timeout: 5000,
+        maximumAge: 0,
+        distanceFilter: 1}
+      );      
     },
     stopLocationUpdates() {
+      this.polyline.setMap(null);
       navigator.geolocation.clearWatch(this.watchPositionId);
     },
 
