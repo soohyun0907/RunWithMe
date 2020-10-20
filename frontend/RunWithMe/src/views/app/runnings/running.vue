76168d25c6c2022f6dae00f8ec2614d3497fffdf
김대용 <kim940813@naver.com>
2020-10-20 15:24:04 +0900

    Start, Stop, Resume, End Running
    Former-commit-id: d3c6b2fb21802b3f2a2cb6b4e891c57b001b2435

--- frontend/RunWithMe/src/views/app/runnings/running.vue
+++ frontend/RunWithMe/src/views/app/runnings/running.vue
@@ -1,42 +1,23 @@
 <template>
   <div class="main-content">
     <section class="top-bar">
-      <breadcumb :page="'Record Running'" :folder="'Runnings'" />
-      <h4>Run With Me?</h4>
-    </section>
+    <breadcumb :page="'Record Running'" :folder="'Runnings'" />
+    <h4>Run With Me?</h4>
+     </section>
 
     <section ref="map" class="map"></section>
 
-    <div class="btn_container">
-      <div v-if="!running">
-        <section class="bottom-bar">
-          <div class="latLngLabel">{{ lat }}, {{ lng }}</div>
-          <button class="running button green" @click="startLocationUpdates">
+    <section class="bottom-bar">
+        <div class="latLngLabel">{{lat}}, {{lng}}</div>
+        <button class="ui button green" @click="startLocationUpdates">
             <i class="i-Start-2"></i>
             Start
-          </button>
-        </section>
-      </div>
-      <div v-if="running">
-        <section class="bottom-bar">
-          <button class="running button red" @click="stopLocationUpdates">
+        </button>
+        <button class="ui button red" @click="stopLocationUpdates">
             <i class="i-Stop-2"></i>
             Stop
-          </button>
-          <button class="running button yellow" @click="startLocationUpdates">
-            <i class="i-Start-2"></i>
-            Resume
-          </button>
-          <button class="running button blue" @click="endLocationUpdates">
-            <i class="i-Start-2"></i>
-            End Running
-          </button>
-        </section>
-      </div>
-    </div>
-    <div id="clock">
-      <span id="time">{{ clock }}</span>
-    </div>
+        </button>
+    </section>
   </div>
 </template>
 <script>
@@ -44,9 +25,10 @@ import SERVER from "@/api/api";
 import axios from "axios";
 
 export default {
+
   metaInfo: {
     // if no subcomponents specify a metaInfo.title, this title will be used
-    title: "Running",
+    title: "Running"
   },
   data() {
       return {
@@ -59,10 +41,10 @@ export default {
       };
   },
   mounted() {
-    if (window.google && window.google.maps) {
+    if(window.google && window.google.maps) {
       this.initMap();
-    } else {
-      const script = document.createElement("script");
+    }else {
+      const script = document.createElement('script');
       script.onload = () => google.maps.load(this.initMap);
     }
   },
@@ -159,23 +141,6 @@ export default {
           // console.log(positions[i].lat);
           runningPathCoordinates.push(new google.maps.LatLng(positions[i].lat, positions[i].lng));
         }
-      this.map = map
-    },
-    endLocationUpdates() {
-      this.running = false;
-      clearInterval(this.started);
-      this.stoppedDuration = 0;
-      this.timeBegan = null;
-      this.timeStopped = null;
-      this.clock = "00:00:00.000";
-    },
-    clockRunning() {
-      var currentTime = new Date();
-      var timeElapsed = new Date(currentTime - this.timeBegan);
-      var hour = timeElapsed.getUTCHours();
-      var min = timeElapsed.getUTCMinutes();
-      var sec = timeElapsed.getUTCSeconds();
-      var ms = timeElapsed.getUTCMilliseconds();
 
         const runningPath = new google.maps.Polyline({
           path: runningPathCoordinates,
@@ -223,45 +188,13 @@ export default {
 }
 .map {
   flex-grow: 1;
-  width: 100%;
-  height: 400px;
-  border: 1px solid red;
+  width:100%;
+  height:400px;
+  border:1px solid red;
 }
 .bottom-bar {
   height: 100px;
   text-align: center;
   background: white;
-}
-/* 스톱워치 디자인 */
-@import url("https://fonts.googleapis.com/css?family=Share+Tech+Mono");
-#clock {
-  order: 0;
-  flex: 0 1 auto;
-  align-self: center;
-
-  color: rgb(200, 200, 200);
-}
-#time {
-  font-size: 6.5em;
-}
-.btn-container {
-  display: flex;
-  margin-top: 15px;
-}
-.running button {
-  text-align: center;
-  font-family: "Share Tech Mono", sans-serif;
-  background: transparent;
-  border: none;
-  color: rgb(200, 200, 200);
-  padding: 10px 15px;
-  margin: 0 10px;
-  text-transform: uppercase;
-  font-size: 2em;
-  cursor: pointer;
-
-  flex-grow: 1;
-
-  transition: color 0.1s ease-out;
 }
 </style>
