84cf9fa3b76c9dc479b276f74282d21c86567476
김대용 <kim940813@naver.com>
2020-10-20 14:23:41 +0900

    StopWatch and Map Line Draws | S03P31A303-48 조치 (이슈 조치를 보기 위해 . 을 입력하세요)
    Former-commit-id: bde3189b4fa12643c1ecdf410f6e964cff60d907

--- frontend/RunWithMe/src/views/app/runnings/kakaomaptest.vue
+++ frontend/RunWithMe/src/views/app/runnings/kakaomaptest.vue
@@ -1,181 +1,297 @@
 <template>
   <div class="main-content">
-    <breadcumb :page="'KakaoMap'" :folder="'Pages'" />
-
-    <div id="map" ref="map"></div>
-
-    <section class="bottom-bar">
-      <div class="latLngLabel">{{ lat }}, {{ lon }}</div>
-      <button class="ui button green" @click="startLocationUpdates">
-        <i class="circle dot outline icon large"></i>
-        Start Location
-      </button>
-      <button class="ui button red" @click="stopLocationUpdates">
-        <i class="circle dot outline icon large"></i>
-        Stop Location
-      </button>
+    <section class="top-bar">
+      <breadcumb :page="'Record Running'" :folder="'Runnings'" />
+      <h4>dy_test?</h4>
     </section>
 
-    <div class="wrap">
-      <h1>5월 마지막주 영화 예매 순위</h1>
+    <section ref="map" class="map"></section>
 
-      <ul class="tests">
-        <li v-for="(test,index) in tests" class="item" :key="index">
-          <span class="rank">{{ test.id }}</span>
-          <router-link :to="{ name: 'show', params: { id: test.id } }">
-            <img v-bind:src="test.poster" class="poster" />
-          </router-link>
-          <div class="detail">
-            <strong class="tit">{{ test.name }}</strong>
-            <span class="rate"
-              >예매율 <span class="num">{{ test.rate }}</span></span
-            >
-            <router-link
-              :to="{ name: 'show', params: { id: test.id } }"
-              class="link"
-              >자세히보기</router-link
-            >
-          </div>
-        </li>
-      </ul>
+    <div class="btn_container">
+      <div v-if="!running">
+        <section class="bottom-bar">
+          <div class="latLngLabel">{{ lat }}, {{ lng }}</div>
+          <button class="running button green" @click="startLocationUpdates">
+            <i class="i-Start-2"></i>
+            Start
+          </button>
+        </section>
+      </div>
+      <div v-if="running">
+        <section class="bottom-bar">
+          <button class="running button red" @click="stopLocationUpdates">
+            <i class="i-Stop-2"></i>
+            Stop
+          </button>
+          <button class="running button yellow" @click="startLocationUpdates">
+            <i class="i-Start-2"></i>
+            Resume
+          </button>
+          <button class="running button blue" @click="endLocationUpdates">
+            <i class="i-Start-2"></i>
+            End Running
+          </button>
+        </section>
+      </div>
+    </div>
+    <div id="clock">
+      <span id="time">{{ clock }}</span>
     </div>
   </div>
 </template>
 <script>
+import SERVER from "@/api/api";
+import axios from "axios";
+
 export default {
-  name: "kakaomap",
-  mounted() {
-    if (window.kakao && window.kakao.maps) {
-      this.initMap();
-    } else {
-      const script = document.createElement("script");
-      script.onload = () => kakao.maps.load(this.initMap);
-      script.src =
-        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=90262fe8162a1bfa04492a90df8f5243";
-      document.head.appendChild(script);
-    }
-  },
-  created() {
-    // 컴포넌트가 생성될 때, /api/tests에 요청을 보냅니다.
-    this.$http.get("/api/tests").then((response) => {
-      this.tests = response.data;
-    });
+  metaInfo: {
+    // if no subcomponents specify a metaInfo.title, this title will be used
+    title: "Running",
   },
   data() {
     return {
-      tests: [],
-      map: null,
       lat: 0,
-      lon: 0,
+      lng: 0,
+      timestamp: 0,
       watchPositionId: null,
-      polyline: {},
+      map: null,
+      previous: [],
+      runningPathCoordinates: [],
+      gpsIdCnts: 0,
+
+      //스톱워치 변수
+      clock: "00:00:00.000",
+      timeBegan: null,
+      timeStopped: null,
+      stoppedDuration: 0,
+      started: null,
+      running: false,
     };
+  },
+  mounted() {
+    if (window.google && window.google.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement("script");
+      script.onload = () => google.maps.load(this.initMap);
+    }
   },
   methods: {
     initMap() {
-      const container = this.$refs.map;
-      var lat = 33.450701,
-        lon = 126.570667;
-      var options = {
-        center: new kakao.maps.LatLng(lat, lon),
-      };
+      var map = new google.maps.Map(this.$refs["map"], {
+        zoom: 15,
+        center: new google.maps.LatLng(37.331777, 127.129347),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+      });
 
-      var map = new kakao.maps.Map(container, options);
-
-      // // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
-      if (navigator.geolocation) {
-        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-        navigator.geolocation.getCurrentPosition(function (position) {
-          lat = position.coords.latitude; // 위도
-          lon = position.coords.longitude; // 경도
-
-          var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-
-          options = {
-            center: new kakao.maps.LatLng(lat, lon),
-          };
-
-          map = new kakao.maps.Map(container, options);
-
-          var circle = new kakao.maps.Circle({
-            center: new kakao.maps.LatLng(lat, lon), // 원의 중심좌표 입니다
-            radius: 50, // 미터 단위의 원의 반지름입니다
-            strokeWeight: 5, // 선의 두께입니다
-            strokeColor: "#75B8FA", // 선의 색깔입니다
-            strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-            strokeStyle: "dashed", // 선의 스타일 입니다
-            fillColor: "#CFE7FF", // 채우기 색깔입니다
-            fillOpacity: 0.7, // 채우기 불투명도 입니다
-          });
-
-          circle.setMap(map);
-          // 지도 중심좌표를 접속위치로 변경합니다
-          map.setCenter(locPosition);
-          // console.log(map);
-        });
-      }
       this.map = map;
-      this.lat = lat;
-      this.lon = lon;
     },
     startLocationUpdates() {
-      var linePath = [];
+      //스톱워치 부분
+      if (this.running) return;
+
+      if (this.timeBegan === null) {
+        this.endLocationUpdates();
+        this.timeBegan = new Date();
+      }
+
+      if (this.timeStopped !== null) {
+        this.stoppedDuration += new Date() - this.timeStopped;
+      }
+
+      this.started = setInterval(this.clockRunning, 10);
+      this.running = true;
+
+      //맵에 기록
+      var map = this.map;
+      var marker = new google.maps.Marker({
+        map: map,
+      });
 
       this.watchPositionId = navigator.geolocation.watchPosition(
         (position) => {
           this.lat = position.coords.latitude;
-          this.lon = position.coords.longitude;
-
+          this.lng = position.coords.longitude;
+          console.log("watchpositionID");
           console.log(position);
+          map.setCenter(new google.maps.LatLng(this.lat, this.lng));
+          marker.setPosition(new google.maps.LatLng(this.lat, this.lng));
 
-          linePath.push(new kakao.maps.LatLng(this.lat, this.lon));
-          const container = this.$refs.map;
-          var options = {
-            center: new kakao.maps.LatLng(this.lat, this.lon),
-          };
-
-          var map = new kakao.maps.Map(container, options);
-          map.setCenter(new kakao.maps.LatLng(this.lat, this.lon));
-
-          // 지도에 표시할 선을 생성합니다
-          var polyline = new kakao.maps.Polyline({
-            path: linePath, // 선을 구성하는 좌표배열 입니다
-            strokeWeight: 5, // 선의 두께 입니다
-            strokeColor: "#FFAE00", // 선의 색깔입니다
-            strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-            strokeStyle: "solid", // 선의 스타일입니다
-          });
-
-          console.log(linePath);
-          // 지도에 선을 표시합니다
-          polyline.setMap(map);
-          this.polyline = polyline;
+          this.runningPathCoordinates.push(
+            new google.maps.LatLng(this.lat, this.lng)
+          );
+          this.savePosition(position);
         },
         (error) => {
           console.log(error.message);
         },
-        { timeout: 5000, maximumAge: 0, distanceFilter: 1 }
+        {
+          timeout: 5000,
+          maximumAge: 0,
+          distanceFilter: 1,
+        }
       );
+      this.map = map
+    },
+    endLocationUpdates() {
+      this.running = false;
+      clearInterval(this.started);
+      this.stoppedDuration = 0;
+      this.timeBegan = null;
+      this.timeStopped = null;
+      this.clock = "00:00:00.000";
+    },
+    clockRunning() {
+      var currentTime = new Date();
+      var timeElapsed = new Date(currentTime - this.timeBegan);
+      var hour = timeElapsed.getUTCHours();
+      var min = timeElapsed.getUTCMinutes();
+      var sec = timeElapsed.getUTCSeconds();
+      var ms = timeElapsed.getUTCMilliseconds();
+
+      this.clock =
+        this.zeroPrefix(hour, 2) +
+        ":" +
+        this.zeroPrefix(min, 2) +
+        ":" +
+        this.zeroPrefix(sec, 2) +
+        "." +
+        this.zeroPrefix(ms, 3);
+    },
+
+    zeroPrefix(num, digit) {
+      var zero = "";
+      for (var i = 0; i < digit; i++) {
+        zero += "0";
+      }
+      return (zero + num).slice(-digit);
     },
     stopLocationUpdates() {
-      this.polyline.setMap(null);
+      this.running = false;
+      this.timeStopped = new Date();
+      clearInterval(this.started);
+
       navigator.geolocation.clearWatch(this.watchPositionId);
+      var paths = [
+        {
+          gpsId: 5,
+          userId: 1,
+          lat: 37.323855,
+          lng: 127.13657,
+          timestamp: 1603106842854,
+        },
+        {
+          gpsId: 6,
+          userId: 1,
+          lat: 37.323855,
+          lng: 127.13657,
+          timestamp: 1603107211013,
+        },
+      ];
+      for (var i = 0; i < paths.length; i++) {
+        this.runningPathCoordinates.push(
+          new google.maps.LatLng(paths[i].lat, paths[i].lng)
+        );
+      }
+      this.drawLines();
+      axios
+        .get(SERVER.URL + "gps/" + 1)
+        .then((res) => {
+          // console.log(res.data);
+          // this.paths = res.data.data
+          // this.drawLines(res.data.data);
+        })
+        .catch((err) => console.log(err.response));
+    },
+    savePosition(position) {
+      let data = {
+        userId: 1,
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        timestamp: position.timestamp,
+      };
+      axios
+        .post(SERVER.URL + "gps", data, {
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+        .then((res) => {
+          console.log("response");
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err.response));
+    },
+    drawLines() {
+      for (var i = 0; i < this.runningPathCoordinates.length; i++) {
+        console.log("drawLines = " + i + this.runningPathCoordinates[i]);
+      }
+
+      const runningPath = new google.maps.Polyline({
+        path: this.runningPathCoordinates,
+        geodesic: true,
+        strokeColor: "#ff0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+      });
+      runningPath.setMap(this.map);
     },
   },
 };
 </script>
-<style scoped>
-#map {
-  width: 300px;
-  height: 310px;
+
+<style>
+.driver-view {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.top-bar {
+  height: 100px;
+  text-align: center;
+  background: white;
+}
+.map {
+  flex-grow: 1;
+  width: 100%;
+  height: 400px;
   border: 1px solid red;
 }
-.app-footer {
-  margin-top: 2rem;
-  background: #eee;
-  padding: 1.25rem;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  display: none;
+.bottom-bar {
+  height: 100px;
+  text-align: center;
+  background: white;
+}
+/* 스톱워치 디자인 */
+@import url("https://fonts.googleapis.com/css?family=Share+Tech+Mono");
+#clock {
+  order: 0;
+  flex: 0 1 auto;
+  align-self: center;
+
+  color: rgb(200, 200, 200);
+}
+#time {
+  font-size: 6.5em;
+}
+.btn-container {
+  display: flex;
+  margin-top: 15px;
+}
+.running button {
+  text-align: center;
+  font-family: "Share Tech Mono", sans-serif;
+  background: transparent;
+  border: none;
+  color: rgb(200, 200, 200);
+  padding: 10px 15px;
+  margin: 0 10px;
+  text-transform: uppercase;
+  font-size: 2em;
+  cursor: pointer;
+
+  flex-grow: 1;
+
+  transition: color 0.1s ease-out;
 }
 </style>
--- frontend/RunWithMe/src/views/app/runnings/running.vue
+++ frontend/RunWithMe/src/views/app/runnings/running.vue
@@ -1,23 +1,42 @@
 <template>
   <div class="main-content">
     <section class="top-bar">
-    <breadcumb :page="'Record Running'" :folder="'Runnings'" />
-    <h4>Run With Me?</h4>
-     </section>
+      <breadcumb :page="'Record Running'" :folder="'Runnings'" />
+      <h4>Run With Me?</h4>
+    </section>
 
     <section ref="map" class="map"></section>
 
-    <section class="bottom-bar">
-        <div class="latLngLabel">{{lat}}, {{lng}}</div>
-        <button class="ui button green" @click="startLocationUpdates">
+    <div class="btn_container">
+      <div v-if="!running">
+        <section class="bottom-bar">
+          <div class="latLngLabel">{{ lat }}, {{ lng }}</div>
+          <button class="running button green" @click="startLocationUpdates">
             <i class="i-Start-2"></i>
             Start
-        </button>
-        <button class="ui button red" @click="stopLocationUpdates">
+          </button>
+        </section>
+      </div>
+      <div v-if="running">
+        <section class="bottom-bar">
+          <button class="running button red" @click="stopLocationUpdates">
             <i class="i-Stop-2"></i>
             Stop
-        </button>
-    </section>
+          </button>
+          <button class="running button yellow" @click="startLocationUpdates">
+            <i class="i-Start-2"></i>
+            Resume
+          </button>
+          <button class="running button blue" @click="endLocationUpdates">
+            <i class="i-Start-2"></i>
+            End Running
+          </button>
+        </section>
+      </div>
+    </div>
+    <div id="clock">
+      <span id="time">{{ clock }}</span>
+    </div>
   </div>
 </template>
 <script>
@@ -25,115 +44,200 @@ import SERVER from "@/api/api";
 import axios from "axios";
 
 export default {
-
   metaInfo: {
     // if no subcomponents specify a metaInfo.title, this title will be used
-    title: "Running"
+    title: "Running",
   },
   data() {
-      return {
-          lat:0,
-          lng:0,
-          timestamp: 0,
-          watchPositionId: null,
-          map: null,
-          previous: [],
-      };
+    return {
+      lat: 0,
+      lng: 0,
+      timestamp: 0,
+      watchPositionId: null,
+      map: null,
+      previous: [],
+      runningPathCoordinates: [],
+      gpsIdCnts: 0,
+
+      //스톱워치 변수
+      clock: "00:00:00.000",
+      timeBegan: null,
+      timeStopped: null,
+      stoppedDuration: 0,
+      started: null,
+      running: false,
+    };
   },
   mounted() {
-    if(window.google && window.google.maps) {
+    if (window.google && window.google.maps) {
       this.initMap();
-    }else {
-      const script = document.createElement('script');
+    } else {
+      const script = document.createElement("script");
       script.onload = () => google.maps.load(this.initMap);
     }
   },
   methods: {
-      initMap(){
-        var map = new google.maps.Map(this.$refs["map"], {
-              zoom: 15,
-              center: new google.maps.LatLng(37.331777, 127.129347),
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
+    initMap() {
+      var map = new google.maps.Map(this.$refs["map"], {
+        zoom: 15,
+        center: new google.maps.LatLng(37.331777, 127.129347),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+      });
 
-        this.map = map;
-      },
-      startLocationUpdates() {
-          this.$refs.map = new google.maps.Map(this.$refs["map"], {
-              zoom: 15,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-          });
-          var marker = new google.maps.Marker({
-              map: map
-          });
+      this.map = map;
+    },
+    startLocationUpdates() {
+      //스톱워치 부분
+      if (this.running) return;
 
-          this.watchPositionId = navigator.geolocation.watchPosition(
-              position => {
-                  this.lat = position.coords.latitude;
-                  this.lng = position.coords.longitude;
-                  console.log(position);
-                  this.$refs.map.setCenter(new google.maps.LatLng(this.lat, this.lng)); 
-                  marker.setPosition(new google.maps.LatLng(this.lat, this.lng));
+      if (this.timeBegan === null) {
+        this.endLocationUpdates();
+        this.timeBegan = new Date();
+      }
 
-                  this.savePosition(position);
-              },
-              error => {
-                  console.log(error.message);
-              },
-              {
-                timeout: 5000,
-                maximumAge: 0
-              }
+      if (this.timeStopped !== null) {
+        this.stoppedDuration += new Date() - this.timeStopped;
+      }
+
+      this.started = setInterval(this.clockRunning, 10);
+      this.running = true;
+
+      //맵에 기록
+      var map = this.map;
+      var marker = new google.maps.Marker({
+        map: map,
+      });
+
+      this.watchPositionId = navigator.geolocation.watchPosition(
+        (position) => {
+          this.lat = position.coords.latitude;
+          this.lng = position.coords.longitude;
+          console.log("watchpositionID");
+          console.log(position);
+          map.setCenter(new google.maps.LatLng(this.lat, this.lng));
+          marker.setPosition(new google.maps.LatLng(this.lat, this.lng));
+
+          this.runningPathCoordinates.push(
+            new google.maps.LatLng(this.lat, this.lng)
           );
-          
-      },
-      stopLocationUpdates() {
-          navigator.geolocation.clearWatch(this.watchPositionId);
-
-          axios.get(SERVER.URL+"gps/"+1)
-          .then(res => {
-            console.log(res.data);
-            this.drawLines(res.data.data);
-          })
-          .catch(err => console.log(err.response));
-      },
-      savePosition(position) {
-          let data = {
-            userId: 1,
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-            timestamp: position.timestamp
-          };
-          axios.post(SERVER.URL+"gps", data, {
-            headers: {
-              "Content-type": "application/json"
-            }
-          })
-            .then(res => {
-              console.log("response")
-              console.log(res.data)
-            })
-            .catch(err => console.log(err.response))
-      },
-      drawLines(positions) {
-        var runningPathCoordinates = [];
-
-        for(var i=0; i<positions.length; i++){
-          console.log(positions[i].lat);
-          runningPathCoordinates.push(new google.maps.LatLng(positions[i].lat, positions[i].lng));
+          this.savePosition(position);
+        },
+        (error) => {
+          console.log(error.message);
+        },
+        {
+          timeout: 5000,
+          maximumAge: 0,
+          distanceFilter: 1,
         }
+      );
+      this.map = map
+    },
+    endLocationUpdates() {
+      this.running = false;
+      clearInterval(this.started);
+      this.stoppedDuration = 0;
+      this.timeBegan = null;
+      this.timeStopped = null;
+      this.clock = "00:00:00.000";
+    },
+    clockRunning() {
+      var currentTime = new Date();
+      var timeElapsed = new Date(currentTime - this.timeBegan);
+      var hour = timeElapsed.getUTCHours();
+      var min = timeElapsed.getUTCMinutes();
+      var sec = timeElapsed.getUTCSeconds();
+      var ms = timeElapsed.getUTCMilliseconds();
 
-        const runningPath = new google.maps.Polyline({
-          path: runningPathCoordinates,
-          geodesic: true,
-          strokeColor: "#ff0000",
-          strokeOpacity: 1.0,
-          strokeWeight: 2,
-        });
-        runningPath.setMap(this.map);
-      },
-  }
-}
+      this.clock =
+        this.zeroPrefix(hour, 2) +
+        ":" +
+        this.zeroPrefix(min, 2) +
+        ":" +
+        this.zeroPrefix(sec, 2) +
+        "." +
+        this.zeroPrefix(ms, 3);
+    },
+
+    zeroPrefix(num, digit) {
+      var zero = "";
+      for (var i = 0; i < digit; i++) {
+        zero += "0";
+      }
+      return (zero + num).slice(-digit);
+    },
+    stopLocationUpdates() {
+      this.running = false;
+      this.timeStopped = new Date();
+      clearInterval(this.started);
+
+      navigator.geolocation.clearWatch(this.watchPositionId);
+      var paths = [
+        {
+          gpsId: 5,
+          userId: 1,
+          lat: 37.323855,
+          lng: 127.13657,
+          timestamp: 1603106842854,
+        },
+        {
+          gpsId: 6,
+          userId: 1,
+          lat: 37.323855,
+          lng: 127.13657,
+          timestamp: 1603107211013,
+        },
+      ];
+      for (var i = 0; i < paths.length; i++) {
+        this.runningPathCoordinates.push(
+          new google.maps.LatLng(paths[i].lat, paths[i].lng)
+        );
+      }
+      this.drawLines();
+      axios
+        .get(SERVER.URL + "gps/" + 1)
+        .then((res) => {
+          // console.log(res.data);
+          // this.paths = res.data.data
+          // this.drawLines(res.data.data);
+        })
+        .catch((err) => console.log(err.response));
+    },
+    savePosition(position) {
+      let data = {
+        userId: 1,
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        timestamp: position.timestamp,
+      };
+      axios
+        .post(SERVER.URL + "gps", data, {
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+        .then((res) => {
+          console.log("response");
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err.response));
+    },
+    drawLines() {
+      for (var i = 0; i < this.runningPathCoordinates.length; i++) {
+        console.log("drawLines = " + i + this.runningPathCoordinates[i]);
+      }
+
+      const runningPath = new google.maps.Polyline({
+        path: this.runningPathCoordinates,
+        geodesic: true,
+        strokeColor: "#ff0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+      });
+      runningPath.setMap(this.map);
+    },
+  },
+};
 </script>
 
 <style>
@@ -149,13 +253,45 @@ export default {
 }
 .map {
   flex-grow: 1;
-  width:100%;
-  height:400px;
-  border:1px solid red;
+  width: 100%;
+  height: 400px;
+  border: 1px solid red;
 }
 .bottom-bar {
   height: 100px;
   text-align: center;
   background: white;
+}
+/* 스톱워치 디자인 */
+@import url("https://fonts.googleapis.com/css?family=Share+Tech+Mono");
+#clock {
+  order: 0;
+  flex: 0 1 auto;
+  align-self: center;
+
+  color: rgb(200, 200, 200);
+}
+#time {
+  font-size: 6.5em;
+}
+.btn-container {
+  display: flex;
+  margin-top: 15px;
+}
+.running button {
+  text-align: center;
+  font-family: "Share Tech Mono", sans-serif;
+  background: transparent;
+  border: none;
+  color: rgb(200, 200, 200);
+  padding: 10px 15px;
+  margin: 0 10px;
+  text-transform: uppercase;
+  font-size: 2em;
+  cursor: pointer;
+
+  flex-grow: 1;
+
+  transition: color 0.1s ease-out;
 }
 </style>
