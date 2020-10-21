cf7785d40c6afbf89a5d147ff523c5e2aab8bcb0
이선수 <tjstndi77@naver.com>
2020-10-21 17:19:22 +0900

    Update vue stomp | S03P31A303-26 Actions (Type . to access issue actions)
    Former-commit-id: c30e2ab54b73cd71c5e75f537161740f528604c2

--- frontend/RunWithMe/src/store/modules/chat.js
+++ frontend/RunWithMe/src/store/modules/chat.js
@@ -6,7 +6,11 @@ const state = {
   contactList: contacts,
   recentUsers: [],
   selectedUser: contacts[0],
-  chats: chatCollection
+  chats: chatCollection,
+  roomInfo : "",
+  messages : ["test", "test1"],
+  roomId : "",
+  roomName : ""
 };
 
 const getters = {
@@ -14,12 +18,18 @@ const getters = {
   getCurrentUser: state => state.currentUser,
   getRecentUser: state => state.recentUsers,
   getSelectedUser: state => state.selectedUser,
-  getUserLists: state => state.getUserLists
+  getUserLists: state => state.getUserLists,
+  getRoomInfo: state => state.roomInfo,
+  getMessages: state => state.messages
 };
 
 const actions = {
   changeSelectedUser({commit}, id) {
     commit("updateSelectedUser", id);
+  },
+  createAndSelectChatroomAction({commit}, uid, sock, ws){
+    commit("createAndSelectChatroom", uid, sock, ws);
+    // commit("test", state.roomInfo);
   }
 };
 
@@ -37,18 +47,39 @@ const mutations = {
         state.contactList = data.data;
       })
   },
-  createAndSelectChatroom : (state, uid) => {
-  
+  createAndSelectChatroom : (state, uid, sock, ws) => {
     http
       .post("/match/room", 
       {
           friendId : uid
       })
       .then((data) =>{
-        console.log(data);
+        //console.log(data);
         state.selectedUser = data.data.name;
+        state.roomInfo = data.data;
+       
+        // state.messages.unshift("add");
+        state.roomId = data.data.roomId;
+        state.roomName =  data.data.roomName;
+        var _this = this;
+        http
+          .get('/chat/user').then(response => {
+              console.log("들어는 오냐")
+              _this.token = response.data.token;
+              console.log("token:" + _this.token)
+              ws.connect({"token":_this.token}, function(frame) {
+                  ws.subscribe("/sub/chat/room/"+_this.roomId, function(message) {
+                      var recv = JSON.parse(message.body);
+                      _this.recvMessage(recv);
+                  });
+              }, function(error) {
+                  alert("서버 연결에 실패 하였습니다. 다시 접속해 주십시요.");
+                  // location.href="/chat/room";
+              });
+        });
       })
   }
+
 };
 
 export default {
