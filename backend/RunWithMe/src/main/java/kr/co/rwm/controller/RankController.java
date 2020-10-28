46760ea52fdbcf2010e4bbeec1648af7b2c35e24
TAEK <hyungtaik@naver.com>
2020-10-28 17:45:06 +0900

    Feat Search Ranking Info | S03P31A303-139
    Former-commit-id: b8defe9a07054d9d251e0a8e2a7f1de4f37ac1cd

---
+++ backend/RunWithMe/src/main/java/kr/co/rwm/controller/RankController.java
@@ -0,0 +1,56 @@
+package kr.co.rwm.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.rwm.entity.Ranks;
+import kr.co.rwm.entity.User;
+import kr.co.rwm.model.Response;
+import kr.co.rwm.model.ResponseMessage;
+import kr.co.rwm.model.StatusCode;
+import kr.co.rwm.service.RanksService;
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ranks")
+public class RankController {
+	private final RanksService rankService;
+	
+	// Top 10 기록 레이스 랭킹 조회
+	@GetMapping("/top/race")
+	public ResponseEntity raceTopRanking() {
+		List<Ranks> result = rankService.raceTop();
+		return new ResponseEntity<Response>(new Response(StatusCode.OK,ResponseMessage.RACE_TOP_RANKING_OK,result),HttpStatus.OK);
+	}
+	
+	// 전체 경험치 순으로 랭킹 조회
+	@GetMapping("/total")
+	public ResponseEntity totalRanking() {
+		List<Ranks> result = rankService.totalRank();
+		return new ResponseEntity<Response>(new Response(StatusCode.OK,ResponseMessage.TOTAL_TOP_RANKING_OK,result),HttpStatus.OK);
+	}
+	
+	// Top10 전체 경험치 순 랭킹 조회
+	@GetMapping("/top/total")
+	public ResponseEntity totalTopRanking() {
+		List<Ranks> result = rankService.totalTop();
+		return new ResponseEntity<Response>(new Response(StatusCode.OK,ResponseMessage.TOTAL_RANKING_OK,result),HttpStatus.OK);
+	}
+
+	// 조건부 검색을 통한 랭킹 정보 조회 (email, userId, userName)
+	@PostMapping("/search")
+	public ResponseEntity searchRankingInfo(@RequestBody User user) {
+		List<Ranks> result = rankService.findByUserId(user);
+		return new ResponseEntity<Response>(new Response(StatusCode.OK,ResponseMessage.TOTAL_RANKING_OK,result),HttpStatus.OK);
+	}
+}
