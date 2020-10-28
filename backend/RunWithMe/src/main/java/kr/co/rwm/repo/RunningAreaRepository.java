a21683ed5fd41cd5a5c983015e7b6c6c994aeb85
Sooni <ksb940925@gmail.com>
2020-10-28 17:06:14 +0900

    Feat running area | S03P31A303-100
    Former-commit-id: 61e8e8818a92751afbf788fbd14e142f95376237

---
+++ backend/RunWithMe/src/main/java/kr/co/rwm/repo/RunningAreaRepository.java
@@ -0,0 +1,9 @@
+package kr.co.rwm.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kr.co.rwm.entity.RunningArea;
+
+public interface RunningAreaRepository extends JpaRepository<RunningArea, Long>{
+
+}
