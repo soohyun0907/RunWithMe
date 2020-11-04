9e766a0c12d292cba6fc1829cf477d73c2e4bda9
전수현 <soohyun0907@naver.com>
2020-11-04 13:26:28 +0900

    Modify main and topNavBar | S03P31A303-63
    Former-commit-id: a8bfdfc3f2fb1055ebeb3c4c7422fd2e3cca7d14

---
+++ frontend/RunWithMe/src/containers/layouts/largeSidebar/footer.vue
@@ -0,0 +1,26 @@
+ <template>
+  <div class="footer_wrap">
+    <div class="app-footer">
+        <div class="d-flex align-items-center">
+          <div>
+            <div>
+              <p class="m-0">&copy; All rights reserved by</p>
+              <p class="m-0">2020 SSAFY3 뛰라노사우루스</p>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</template> 
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style> 
