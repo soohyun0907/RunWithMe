5b280bbf1bc52c8d7f8905de34734f294c0bd22b
이선수 <tjstndi77@naver.com>
2020-11-04 20:17:36 +0900

    Update view (delete social login)
    Former-commit-id: 5aed568807b00c0d09815caadbf248d833d57dd3

--- frontend/RunWithMe/src/views/app/sessions/signIn.vue
+++ frontend/RunWithMe/src/views/app/sessions/signIn.vue
@@ -69,25 +69,6 @@
             style="backgroundsize: cover"
             :style="{ backgroundImage: 'url(' + signInImage + ')' }"
           >
-            <div class="pr-3 auth-right">
-              <router-link
-                to="signUp"
-                class="btn btn-rounded btn-outline-primary btn-outline-email btn-block btn-icon-text"
-                href="signup.html"
-              >
-                <i class="i-Mail-with-At-Sign"></i> Sign up with Email
-              </router-link>
-              <a
-                class="btn btn-rounded btn-outline-primary btn-outline-google btn-block btn-icon-text"
-              >
-                <i class="i-Google-Plus"></i> Sign up with Google
-              </a>
-              <a
-                class="btn btn-rounded btn-outline-primary btn-block btn-icon-text btn-outline-facebook"
-              >
-                <i class="i-Facebook-2"></i> Sign up with Facebook
-              </a>
-            </div>
           </b-col>
         </div>
       </div>
