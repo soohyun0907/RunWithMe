14b6827c6c4d23b8505222a94451ee8b6d62867e
Ubuntu <ubuntu@ip-172-26-0-217.ap-northeast-2.compute.internal>
2020-11-04 16:37:33 +0000

    Fix error and modify url, etc
    Former-commit-id: 768541be215c6b10d46255a2f57b82a1211fa56d

--- frontend/RunWithMe/src/utils/http-common.js
+++ frontend/RunWithMe/src/utils/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: "http://localhost:8080/",
+    baseURL: "http://k3a303.p.ssafy.io:8080/",
   // baseURL: "http://fe8330147a8b.ngrok.io/",
     headers: {
       AUTH:localStorage.getItem("auth")
