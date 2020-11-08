767ed619a94cba6c794f4a91c252fe4b752ea024
TAEK <hyungtaik@naver.com>
2020-11-09 01:43:21 +0900

    Update Delete User | S03P31A303-84
    Former-commit-id: 4bc69a5eb993285181d65a622b593f6624620479 [formerly 6d79462a9cf4c6c29735148c71c3704192055bca]
    Former-commit-id: 0ae9044d074c2c7a3446d298fa443e802c1a668d

--- backend/RunWithMe/src/main/java/kr/co/rwm/repo/RunningUserRepository.java
+++ backend/RunWithMe/src/main/java/kr/co/rwm/repo/RunningUserRepository.java
@@ -8,5 +8,6 @@ import kr.co.rwm.entity.User;
 public interface RunningUserRepository extends JpaRepository<RunningUser, Long> {
 
 	RunningUser findByUserId(User user);
+	void deleteByUserId(User user);
 
 }
