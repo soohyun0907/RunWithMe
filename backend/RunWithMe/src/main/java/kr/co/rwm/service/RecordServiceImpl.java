b694b00398a8752065880e707d6faeb657f1072a
Sooni <ksb940925@gmail.com>
2020-11-09 13:35:52 +0900

    Modify RunningUser distance
    Former-commit-id: 474ffef4c9b0d094c281dc95a2f9a5d36bfef35c [formerly 0b5b5e52485277315423ce37a9528ed20c00e39a]
    Former-commit-id: 15c9a19c2b41b97f6dbc0d3d6597865a8bcedf3a

--- backend/RunWithMe/src/main/java/kr/co/rwm/service/RecordServiceImpl.java
+++ backend/RunWithMe/src/main/java/kr/co/rwm/service/RecordServiceImpl.java
@@ -94,7 +94,7 @@ public class RecordServiceImpl implements RecordService {
 		Running running = runningRepository.findByRunningId(runningId).get();
 		RunningUser runningUser = runningUserRepository.findByUserId(user);
 		runningUser.setTotalCount(runningUser.getTotalCount()-1);
-		runningUser.setTotalDistane(runningUser.getTotalDistane()-running.getAccDistance());
+		runningUser.setTotalDistance(runningUser.getTotalDistance()-running.getAccDistance());
 		runningUser.setTotalTime(runningUser.getTotalTime()-running.getAccTime());
 		runningUserRepository.save(runningUser);
 		
@@ -152,7 +152,7 @@ public class RecordServiceImpl implements RecordService {
 	public void join(User user) {
 		RunningUser runningUser = RunningUser.builder()
 											.userId(user)
-											.totalDistane(0)
+											.totalDistance(0)
 											.totalTime(0L)
 											.totalCount(0)
 											.build();
@@ -180,7 +180,7 @@ public class RecordServiceImpl implements RecordService {
 	public RunningUser updateRunningUser(User user, Running running) {
 		RunningUser runningUser = runningUserRepository.findByUserId(user);
 		runningUser.setTotalCount(runningUser.getTotalCount()+1);
-		runningUser.setTotalDistane(runningUser.getTotalDistane()+running.getAccDistance());
+		runningUser.setTotalDistance(runningUser.getTotalDistance()+running.getAccDistance());
 		runningUser.setTotalTime(runningUser.getTotalTime()+running.getAccTime());
 		
 		return runningUserRepository.save(runningUser);
