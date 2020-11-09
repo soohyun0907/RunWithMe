d7339459aefa797ed06037975be64691eae32510
전수현 <soohyun0907@naver.com>
2020-11-09 11:31:36 +0900

    Modify sideNavBar
    Former-commit-id: a5bc29a314e140a7f3b7966e9df99c0096689d43 [formerly 3d2f6c40b43b507d5df4ecc7763ef8b6cd1f9a0b]
    Former-commit-id: 4f0716688fd13ab74db42baba24bf3399a790efe

--- frontend/RunWithMe/src/containers/layouts/largeSidebar/Sidebar.vue
+++ frontend/RunWithMe/src/containers/layouts/largeSidebar/Sidebar.vue
@@ -181,16 +181,16 @@
               <span class="item-name">챌린지 제안</span>
             </router-link>
           </li>
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <router-link tag="a" class to="/app/board/challengeBoardDetail">
               <i class="nav-icon i-Stopwatch"></i>
               <span class="item-name">챌린지 제안 상세</span>
             </router-link>
-          </li>
+          </li> -->
           <li class="nav-item">
             <router-link tag="a" class to="/app/board/createChallenge">
               <i class="nav-icon i-Speach-Bubble-3"></i>
-              <span class="item-name">챌린지 생성</span>
+              <span class="item-name">챌린지 생성(관리자만)</span>
             </router-link>
           </li>
         </ul>
