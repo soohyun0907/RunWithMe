e4bc83d1b71b83fb6aec041532e172f4b2851695
전수현 <soohyun0907@naver.com>
2020-11-12 10:49:09 +0900

    Feat challenge admin page CRUD | S03P31A303-132
    Former-commit-id: 0ec4a5cc8d0f1c3556615bf9ae03c635782c7513 [formerly fc9fd7a8c9bd611aece417f6659189ea0652965f]
    Former-commit-id: 44f75655a8f516e02f969631325e1f50430ba16b

--- frontend/RunWithMe/src/views/app/board/challengesAdmin.vue
+++ frontend/RunWithMe/src/views/app/board/challengesAdmin.vue
@@ -89,7 +89,8 @@ export default {
         }
       ],
       rows: [],
-      deleteModal: ""
+      deleteModal: "",
+      alertModal: ""
     };
   },
   computed: {
@@ -98,6 +99,27 @@ export default {
   mounted() {
     this.getChallenges();
     this.$store.commit('closeSidebar')
+    console.log(this.userInfo.roles.length);
+    if(this.userInfo.roles.length == 1){
+      this.alertModal = "";
+      this.$bvModal
+        .msgBoxConfirm("관리자만 접근 가능한 페이지입니다.", {
+          size: "sm",
+          buttonSize: "sm",
+          okVariant: "danger",
+          okTitle: "YES",
+          footerClass: "p-2",
+          hideHeaderClose: false,
+          centered: true
+        })
+        .then(value => {
+          this.alertModal = value;
+          this.$router.push("/app/dashboards/main");
+        })
+        .catch(err => {
+          console.log(error);
+        });
+    }
   },
   methods: {
     ...mapMutations(["closeSidebar"]),
