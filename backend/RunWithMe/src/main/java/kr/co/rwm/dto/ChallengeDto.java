90a16a12face53f6c1b75a4e29c442b1b92816de
Sooni <ksb940925@gmail.com>
2020-11-12 13:12:08 +0900

    Feat Dto | S03P31A303-155
    Former-commit-id: d015a0d7a33bac9118e1e04d921bb1f1b5e2418a [formerly b2b20da53d91a1cca89b2dbf1322d070f39df2fc]
    Former-commit-id: efff6f8a4af2fa99621531be95603cba90994934

--- backend/RunWithMe/src/main/java/kr/co/rwm/dto/ChallengeDto.java
+++ backend/RunWithMe/src/main/java/kr/co/rwm/dto/ChallengeDto.java
@@ -2,17 +2,8 @@ package kr.co.rwm.dto;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Transient;
-
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
--- backend/RunWithMe/src/main/java/kr/co/rwm/dto/MessageDto.java
+++ backend/RunWithMe/src/main/java/kr/co/rwm/dto/MessageDto.java
@@ -2,16 +2,8 @@ package kr.co.rwm.dto;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
