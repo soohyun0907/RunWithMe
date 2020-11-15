edbafc046f816f25a96b86cf4615db912214e7f1
전수현 <soohyun0907@naver.com>
2020-11-15 21:41:33 +0900

    Modify some errors
    Former-commit-id: 52eed00d2f3a2a0d78f8a6869a600abbc9a2b162 [formerly 9deeb513c44a88464ac4ef3ce53a8df541b22746]
    Former-commit-id: af7f305ce14b43ead566920536e10f0851e7c4d3

--- frontend/RunWithMe/src/views/app/board/challenges.vue
+++ frontend/RunWithMe/src/views/app/board/challenges.vue
@@ -128,14 +128,14 @@
                                 <img :src="challenge.img" />
                                 <p> 기간: {{ challenge.startTime | moment('YYYY.MM.DD') }} ~ {{ challenge.endTime | moment('YYYY.MM.DD') }} </p>
                                 <p> {{ challenge.content }} </p>
-                                <h6>모인 금액</h6>
+                                <h6>모인 금액 {{ challenge.donateCurrent }} / {{ challenge.donateGoal }} 원</h6>
                                 <b-progress class="mb-3"
                                     variant="success"
                                     :max="challenge.donateGoal"
                                     :value="challenge.donateCurrent"
                                     animated show-progress>
                                 </b-progress>
-                                <h6>전체 달성률</h6>
+                                <h6>전체 달성률 {{ challenge.distanceCurrent }} / {{ challenge.distanceGoal }} KM </h6>
                                 <b-progress class="mb-3"
                                     variant="warning"
                                     :max="challenge.distanceGoal"
@@ -279,6 +279,7 @@ export default {
                 .then(({data}) => {
                     if(data.status==200){
                         let obj;
+                        console.log(data.data);
                         data.data.forEach(element => {
                             obj = new Object();
                             obj.challengeId = element.challengeId;
