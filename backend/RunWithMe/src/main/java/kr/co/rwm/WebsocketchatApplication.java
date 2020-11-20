2a4190f98654a4fece93eb1f4fffee58790fe753
Sooni <ksb940925@gmail.com>
2020-11-20 11:18:32 +0900

    Modify Scheduled
    Former-commit-id: 5d272eb65ac1c854ee796703d21c368f8af44e26 [formerly e8a2c0bd8376ceb04a4f0cd28b08e8680998ee47] [formerly 85629f48e094aabb0ec79b3cb6a8d0ce4c704bde [formerly fc1e033fa8d8a3047ab3223c5a50cbce2798097e]]
    Former-commit-id: ff21d4b99e02c02c01f2e2ef0c03eef8f6390194 [formerly 5a1657ded6163278419625673509a54feaea538a]
    Former-commit-id: 1cb3534c9c1df28fca5efef3d15ef460488340fd

--- backend/RunWithMe/src/main/java/kr/co/rwm/WebsocketchatApplication.java
+++ backend/RunWithMe/src/main/java/kr/co/rwm/WebsocketchatApplication.java
@@ -4,8 +4,10 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WebsocketchatApplication {
 
     public static void main(String[] args) {
